c5a6132c5c277b2ff98f4401f0a7d4c1355636ac
PedroC-Pires <you@example.com>
2024-04-09 20:41:57 -0300

    1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+let scrollPos = 0;
+let scrollHeight = document.getElementById("html").offsetHeight - document.getElementById("html").clientHeight;
+let scrollPosPercentage = 0;
+function bgScroll() {
+    scrollPos = window.pageYOffset;
+    scrollPosPercentage = 100 * scrollPos / scrollHeight
+
+    document.body.style.backgroundPositionY = scrollPosPercentage + "%";
+    
+    setTimeout(bgScroll, 5);
+}
+bgScroll()
