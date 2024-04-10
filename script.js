5f7236ec6efd49a76138b2f4f7dd9fc3c01616bb
PEDRO DA CONCEIÇÃO PIRES <pedro_c_pires@estudante.sesisenai.org.br>
2024-04-10 09:04:57 -0300

    1.6

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// Declare all variables
 let scrollPos = 0;
-let scrollHeight = document.getElementById("html").offsetHeight - document.getElementById("html").clientHeight;
+let scrollHeight = 0;
 let scrollPosPercentage = 0;
-function bgScroll() {
-    scrollPos = window.pageYOffset;
-    scrollPosPercentage = 100 * scrollPos / scrollHeight
+let setScrollPos = document.body.style.backgroundPositionY;
 
-    document.body.style.backgroundPositionY = scrollPosPercentage + "%";
-    
-    setTimeout(bgScroll, 10);
+// Starts function bgScroll
+function bgScroll() {
+    scrollHeight = document.getElementById("html").offsetHeight - document.getElementById("html").clientHeight;
+    scrollPos = window.pageYOffset;                         // Get scroll position in pixels
+    scrollPosPercentage = 100 * scrollPos / scrollHeight    // Convert the scroll position to a percentage
+    document.body.style.backgroundPositionY = scrollPosPercentage + "%";    // Set the background Y position
+    setTimeout(bgScroll, 1000/60);                             // Call the function bgScroll 60 times per second
 }
 bgScroll()
