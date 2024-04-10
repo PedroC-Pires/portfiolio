c87d80d95982a597b0d719990d1bab1a362512dc
Pedro <127866553+PedroC-Pires@users.noreply.github.com>
2024-04-10 11:53:37 -0300

    1.6

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 // Declare all variables
 let scrollPos = 0;
 let scrollHeight = 0;
+let scrollWidth = 0;
 let scrollPosPercentage = 0;
-let setScrollPos = document.body.style.backgroundPositionY;
 
-// Starts function bgScroll
+// Starts the function bgScroll
 function bgScroll() {
+    // Get page width and page height
+    scrollWidth = document.getElementById("html").offsetWidth - document.getElementById("html").clientWidth;
     scrollHeight = document.getElementById("html").offsetHeight - document.getElementById("html").clientHeight;
-    scrollPos = window.pageYOffset;                         // Get scroll position in pixels
-    scrollPosPercentage = 100 * scrollPos / scrollHeight    // Convert the scroll position to a percentage
-    document.body.style.backgroundPositionY = scrollPosPercentage + "%";    // Set the background Y position
-    setTimeout(bgScroll, 1000/60);                             // Call the function bgScroll 60 times per second
+
+    // Scrolls the background vertically if the viewport is widescreen
+    if(scrollWidth > scrollHeight){
+        scrollPos = window.pageYOffset;
+        scrollPosPercentage = 100 * scrollPos / scrollHeight
+        document.body.style.backgroundPositionY = scrollPosPercentage + "%";
+    } // Scrolls the background horizontally if the viewport is NOT widescreen
+    else{
+        scrollPos = window.pageYOffset;
+        scrollPosPercentage = 100 * scrollPos / scrollHeight
+        document.body.style.backgroundPositionX = scrollPosPercentage + "%";
+    }
+
+    // RUns bgScroll at 60 fps
+    setTimeout(bgScroll, 1000/60);
 }
 bgScroll()
