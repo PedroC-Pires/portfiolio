28723352b6effd6f6dfdd5d40385a8c65459adda
PedroC-Pires <you@example.com>
2024-04-10 20:32:54 -0300

    1.8

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ let scrollHeight = 0;
 let scrollPosPercentage = 0;
 let htmlTag = document.getElementById("html");
 
+if(sessionStorage.getItem("lightTheme")==null){
+    sessionStorage.setItem("lightTheme", "true");
+}
+
+if(sessionStorage.getItem("lightTheme")=="true"){
+    document.getElementById("themeIcon").src = "./img/moonicon.png";
+    document.getElementById("styleTag").href = "./style/style.css";
+} else{
+    document.getElementById("themeIcon").src = "./img/sunicon.png";
+    document.getElementById("styleTag").href = "./style/style_alt.css";
+}
+
 function bgScroll() {
     scrollHeight = htmlTag.offsetHeight - htmlTag.clientHeight;
     scrollPos = window.pageYOffset;
@@ -11,4 +23,17 @@ function bgScroll() {
     document.body.style.backgroundPositionX = scrollPosPercentage + "%";
     setTimeout(bgScroll, 1000/60);
 }
+
+function themeSwitch(){
+    if(sessionStorage.getItem("lightTheme")=="false"){
+        document.getElementById("themeIcon").src = "./img/moonicon.png";
+        document.getElementById("styleTag").href = "./style/style.css";
+        sessionStorage.setItem("lightTheme", "true");
+    } else{
+        document.getElementById("themeIcon").src = "./img/sunicon.png";
+        document.getElementById("styleTag").href = "./style/style_alt.css";
+        sessionStorage.setItem("lightTheme", "false");
+    }
+}
+
 bgScroll()
