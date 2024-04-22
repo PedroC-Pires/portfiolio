21ecc4d49fa86a6c4e37e390fb63638042023c7e
PEDRO DA CONCEIÇÃO PIRES <pedro_c_pires@estudante.sesisenai.org.br>
2024-04-22 11:14:01 -0300

    2.0

--- script/script.js
+++ script/script.js
@@ -3,29 +3,12 @@ let scrollHeight = 0;
 let scrollPosPercentage = 0;
 let htmlTag = document.getElementById("html");
 
-if(sessionStorage.getItem("lightTheme")==null){
-    sessionStorage.setItem("lightTheme", "true");
-}
-
-if(sessionStorage.getItem("lightTheme")=="true"){
-    document.getElementById("themeIcon").src = "./img/moonicon.png";
-    document.getElementById("styleTag").href = "./style/style.css";
-} else{
-    document.getElementById("themeIcon").src = "./img/sunicon.png";
-    document.getElementById("styleTag").href = "./style/style_alt.css";
-}
-
 function bgScroll() {
     scrollHeight = htmlTag.offsetHeight - htmlTag.clientHeight;
     scrollPos = window.pageYOffset;
     scrollPosPercentage = 100 * scrollPos / scrollHeight
-    if(htmlTag.offsetWidth > htmlTag.clientHeight){
-        document.body.style.backgroundSize = "100% auto"
-        document.body.style.backgroundPositionY = scrollPosPercentage + "%";
-    } else{
-        document.body.style.backgroundSize = "auto 100%"
-        document.body.style.backgroundPositionX = scrollPosPercentage + "%";
-    }
+    document.body.style.backgroundPositionY = scrollPosPercentage + "%";
+    document.body.style.backgroundPositionX = scrollPosPercentage + "%";
     setTimeout(bgScroll, 1000/60);
 }
 
@@ -41,4 +24,11 @@ function themeSwitch(){
     }
 }
 
+if(sessionStorage.getItem("lightTheme") == null){
+    sessionStorage.setItem("lightTheme", "");
+}
+
+sessionStorage.setItem("lightTheme", "false");
+
+themeSwitch();
 bgScroll()
