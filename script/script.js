e51a6f953034ab106d9f8b9eab513ed5230e813b
Pedro Pires <pedrocpires25@gmail.com>
2024-04-28 16:07:50 -0300

    2.1

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ let scrollPos = 0;
 let scrollHeight = 0;
 let scrollPosPercentage = 0;
 let htmlTag = document.getElementById("html");
+let currentBackgroundSize = 1080;
 
 if(sessionStorage.getItem("lightTheme")==null){
     sessionStorage.setItem("lightTheme", "true");
@@ -11,10 +12,12 @@ if(sessionStorage.getItem("lightTheme")=="true"){
     document.getElementById("styleTag").href = "./style/style_alt.css";
     document.getElementById("themeIcon").src = "./img/moonicon.png";
     document.getElementById("styleTag").href = "./style/style.css";
+    document.body.style.backgroundImage = "url(../img/background/bgnighttime" + currentBackgroundSize + ".png)";
 } else{
     document.getElementById("styleTag").href = "./style/style.css";
     document.getElementById("themeIcon").src = "./img/sunicon.png";
     document.getElementById("styleTag").href = "./style/style_alt.css";
+    document.body.style.backgroundImage = "url(../img/background/bgdaytime" + currentBackgroundSize + ".png)";
 }
 
 function bgScroll() {
@@ -27,15 +30,39 @@ function bgScroll() {
 }
 
 function themeSwitch(){
+    checkRes();
     if(sessionStorage.getItem("lightTheme")=="false"){
         document.getElementById("themeIcon").src = "./img/moonicon.png";
         document.getElementById("styleTag").href = "./style/style.css";
+        document.body.style.backgroundImage = "url(../img/background/bgnighttime" + currentBackgroundSize + ".png)";
         sessionStorage.setItem("lightTheme", "true");
     } else{
         document.getElementById("themeIcon").src = "./img/sunicon.png";
         document.getElementById("styleTag").href = "./style/style_alt.css";
+        document.body.style.backgroundImage = "url(../img/background/bgdaytime" + currentBackgroundSize + ".png)";
         sessionStorage.setItem("lightTheme", "false");
     }
 }
 
-bgScroll()
+function checkRes(){
+    if(htmlTag.clientWidth > htmlTag.clientHeight){
+        if(htmlTag.clientHeight <= 1080){
+            currentBackgroundSize = 1080;
+        } else if(htmlTag.clientHeight > 1080 && htmlTag.clientHeight <= 1440){
+            currentBackgroundSize = 1440;
+        } else if(htmlTag.clientHeight > 1440){
+            currentBackgroundSize = 2160;
+        }
+    } else {
+        if(htmlTag.clientWidth <= 1080){
+            currentBackgroundSize = 1080;
+        } else if(htmlTag.clientWidth > 1080 && htmlTag.clientWidth <= 1440){
+            currentBackgroundSize = 1440;
+        } else if(htmlTag.clientWidth > 1440){
+            currentBackgroundSize = 2160;
+        }
+    }
+}
+
+checkRes();
+bgScroll();
